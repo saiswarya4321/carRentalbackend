1b58035726cfc562331c6840ae6fa0ea8e033d35
saiswarya4321 <aiswaryas4906@gmail.com>
2025-05-02 13:25:38 +0530

    second commit

--- index.js
+++ index.js
@@ -14,9 +14,24 @@ require('dotenv').config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) // ✅ important for form-data parsing
 
-app.use(cors({origin: 'http://localhost:5173',
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://car-rental-frontend-amber.vercel.app'
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'));
+    },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'] }));
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD']
+  }));
+  
 
     app.use(cookieparser());
     connectionDB();
